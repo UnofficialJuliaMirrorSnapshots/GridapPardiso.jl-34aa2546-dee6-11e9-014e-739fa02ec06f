1c5e9321fbd4fcb54250e6dafbf5aa0f924ae250
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-10-30 12:21:21 +0000

    Snapshot of branch master taken on 2019-10-30T08:21:20.335-04:00 from "https://github.com/UnofficialJuliaMirror/GridapPardiso.jl-34aa2546-dee6-11e9-014e-739fa02ec06f"

--- test/LinearSolver.jl
+++ test/LinearSolver.jl
@@ -4,30 +4,191 @@ using Gridap
 using GridapPardiso
 using Test
 using SparseArrays
+using SparseMatricesCSR
 
 tol = 1.0e-13
 
-# pardiso! (solving the transpose of the system above)
-A = sparse(Vector{Int32}([1,2,3,4,5]),Vector{Int32}([1,2,3,4,5]),Vector{Float64}([1.0,2.0,3.0,4.0,5.0]))
-b = ones(A.n)
+#####################################################
+# SparseMatrixCSC
+#####################################################
+#
+# Matrix from Intel MKL Pardiso examples
+#
+#        DATA ia /1,4,6,9,12,14/
+#        DATA ja 
+#     1  /1,2,  4,
+#     2   1,2,
+#     3       3,4,5,
+#     4   1,  3,4,
+#     5     2,    5/
+#        DATA a
+#     1  / 1.d0,-1.d0,      -3.d0,
+#     2   -2.d0, 5.d0,
+#     3                4.d0, 6.d0, 4.d0,
+#     4   -4.d0,       2.d0, 7.d0,
+#     5          8.d0,            -5.d0/
+#####################################################
+I_ = [1,1,1,2,2,3,3,3,4,4,4,5,5] 
+J_ = [1,2,4,1,2,3,4,5,1,3,4,2,5]
+V_ = [1,-1,-3,-2,5,4,6,4,-4,2,7,8,-5]
+rows = 5
+cols = 5
+
+# pardiso!
+I = Vector{Int32}(); J = Vector{Int32}(); V = Vector{Float64}()
+for (ik, jk, vk) in zip(I_,J_,V_)
+    push_coo!(SparseMatrixCSC,I,J,V,ik,jk,vk)
+end
+finalize_coo!(SparseMatrixCSC,I,J,V,rows, cols)
+A = sparse(I,J,V,rows,cols)
+b = ones(size(A)[2])
 x = similar(b)
 ps = PardisoSolver(GridapPardiso.MTYPE_REAL_NON_SYMMETRIC, new_iparm(A), GridapPardiso.MSGLVL_VERBOSE)
 ss = symbolic_setup(ps, A)
 ns = numerical_setup(ss, A)
 solve!(x, ns, b)
-@test maximum(abs.(A'*x-b)) < tol
+@test maximum(abs.(A*x-b)) < tol
 test_linear_solver(ps, A, b, x)
 
 if Int == Int64
-    # pardiso_64! (solving the transpose of the system above)
-    A = sparse([1,2,3,4,5],[1,2,3,4,5],[1.0,2.0,3.0,4.0,5.0])
-    b = ones(A.n)
+    # pardiso_64!
+    I = Vector{Int64}(); J = Vector{Int64}(); V = Vector{Float64}()
+    for (ik, jk, vk) in zip(I_,J_,V_)
+        push_coo!(SparseMatrixCSC,I,J,V,ik,jk,vk)
+    end
+    finalize_coo!(SparseMatrixCSC,I,J,V,rows, cols)
+    A = sparse(I,J,V,rows, cols)
+    b = ones(size(A)[2])
     x = similar(b)
     ps = PardisoSolver(GridapPardiso.MTYPE_REAL_NON_SYMMETRIC, new_iparm(A), GridapPardiso.MSGLVL_VERBOSE)
     ss = symbolic_setup(ps, A)
     ns = numerical_setup(ss, A)
     solve!(x, ns, b)
-    @test maximum(abs.(A'*x-b)) < tol
+    @test maximum(abs.(A*x-b)) < tol
+    test_linear_solver(ps, A, b, x)
+end
+
+#####################################################
+# SparseMatrixCSR
+#####################################################
+#
+# Matrix from Intel MKL Pardiso examples
+#
+#        DATA ia /1,4,6,9,12,14/
+#        DATA ja 
+#     1  /1,2,  4,
+#     2   1,2,
+#     3       3,4,5,
+#     4   1,  3,4,
+#     5     2,    5/
+#        DATA a
+#     1  / 1.d0,-1.d0,      -3.d0,
+#     2   -2.d0, 5.d0,
+#     3                4.d0, 6.d0, 4.d0,
+#     4   -4.d0,       2.d0, 7.d0,
+#     5          8.d0,            -5.d0/
+#####################################################
+I_ = [1,1,1,2,2,3,3,3,4,4,4,5,5] 
+J_ = [1,2,4,1,2,3,4,5,1,3,4,2,5]
+V_ = [1,-1,-3,-2,5,4,6,4,-4,2,7,8,-5]
+rows = 5
+cols = 5
+
+# pardiso! 
+I = Vector{Int32}(); J = Vector{Int32}(); V = Vector{Float64}()
+for (ik, jk, vk) in zip(I_,J_,V_)
+    push_coo!(SparseMatrixCSR,I,J,V,ik,jk,vk)
+end
+finalize_coo!(SparseMatrixCSR,I,J,V,rows, cols)
+A = sparsecsr(I,J,V,rows, cols)
+b = ones(size(A)[2])
+x = similar(b)
+ps = PardisoSolver(GridapPardiso.MTYPE_REAL_NON_SYMMETRIC, new_iparm(A), GridapPardiso.MSGLVL_VERBOSE)
+ss = symbolic_setup(ps, A)
+ns = numerical_setup(ss, A)
+solve!(x, ns, b)
+@test maximum(abs.(A*x-b)) < tol
+test_linear_solver(ps, A, b, x)
+
+if Int == Int64
+    # pardiso_64!
+    I = Vector{Int64}(); J = Vector{Int64}(); V = Vector{Float64}()
+    for (ik, jk, vk) in zip(I_,J_,V_)
+        push_coo!(SparseMatrixCSR,I,J,V,ik,jk,vk)
+    end
+    finalize_coo!(SparseMatrixCSR,I,J,V,rows, cols)
+    A = sparsecsr(I,J,V,rows, cols)
+    b = ones(size(A)[2])
+    x = similar(b)
+    ps = PardisoSolver(GridapPardiso.MTYPE_REAL_NON_SYMMETRIC, new_iparm(A), GridapPardiso.MSGLVL_VERBOSE)
+    ss = symbolic_setup(ps, A)
+    ns = numerical_setup(ss, A)
+    solve!(x, ns, b)
+    @test maximum(abs.(A*x-b)) < tol
+    test_linear_solver(ps, A, b, x)
+end
+
+#####################################################
+# SymSparseMatrixCSR
+#####################################################
+#
+# Matrix from Intel MKL Pardiso examples
+#
+# ia = (/ 1, 5, 8, 10, 12, 15, 17, 18, 19 /)
+# ja = (/ 1,    3,       6, 7,    &
+#            2, 3,    5,          &
+#               3,             8, &
+#                  4,       7,    &
+#                     5, 6, 7,    &
+#                        6,    8, &
+#                           7,    &
+#                              8 /)
+# a = (/ 7.d0,        1.d0,             2.d0, 7.d0,        &
+#              -4.d0, 8.d0,       2.d0,                    &
+#                     1.d0,                         5.d0,  &
+#                           7.d0,             9.d0,        &
+#                                 5.d0, 1.d0, 5.d0,        &
+#                                      -1.d0,       5.d0,  &
+#                                            11.d0,        &
+#                                                   5.d0 /)
+#####################################################
+I_ = [1,1,1,1,2,2,2,3,3,4,4,5,5,5,6,6,7,8] 
+J_ = [1,3,6,7,2,3,5,3,8,4,7,5,6,7,6,8,7,8]
+V_ = [7,1,2,7,-4,8,2,1,5,7,9,5,1,5,-1,5,11,5]
+rows = 8
+cols = 8
+
+# pardiso! 
+I = Vector{Int32}(); J = Vector{Int32}(); V = Vector{Float64}()
+for (ik, jk, vk) in zip(I_,J_,V_)
+    push_coo!(SymSparseMatrixCSR,I,J,V,ik,jk,vk)
+end
+finalize_coo!(SymSparseMatrixCSR,I,J,V,rows,cols)
+A = symsparsecsr(I,J,V,rows,cols)
+b = ones(size(A)[2])
+x = similar(b)
+ps = PardisoSolver(GridapPardiso.MTYPE_REAL_SYMMETRIC_INDEFINITE, new_iparm(A), GridapPardiso.MSGLVL_VERBOSE)
+ss = symbolic_setup(ps, A)
+ns = numerical_setup(ss, A)
+solve!(x, ns, b)
+@test maximum(abs.(A*x-b)) < tol
+test_linear_solver(ps, A, b, x)
+
+if Int == Int64
+    # pardiso_64!
+    I = Vector{Int64}(); J = Vector{Int64}(); V = Vector{Float64}()
+    for (ik, jk, vk) in zip(I_,J_,V_)
+        push_coo!(SymSparseMatrixCSR,I,J,V,ik,jk,vk)
+    end
+    finalize_coo!(SymSparseMatrixCSR,I,J,V,rows,cols)
+    A = symsparsecsr(I,J,V,rows,cols)
+    b = ones(size(A)[2])
+    x = similar(b)
+    ps = PardisoSolver(GridapPardiso.MTYPE_REAL_SYMMETRIC_INDEFINITE, new_iparm(A), GridapPardiso.MSGLVL_VERBOSE)
+    ss = symbolic_setup(ps, A)
+    ns = numerical_setup(ss, A)
+    solve!(x, ns, b)
+    @test maximum(abs.(A*x-b)) < tol
     test_linear_solver(ps, A, b, x)
 end
 
